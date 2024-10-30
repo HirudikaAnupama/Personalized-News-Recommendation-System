94421a38349acb1ac1c990b088f8b3c1076a56ab
Your NameHirudika <you@example.comhirudikaanupama4@gmail.com>
2024-10-30 11:56:31 +0530

    Create userManagement and articleManagement classes

---
+++ src/main/java/org/example/personalizednewsrecommendationsystem/ArticleManagement.java
@@ -0,0 +1,12 @@
+package org.example.personalizednewsrecommendationsystem;
+
+public class ArticleManagement {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+
+
+}
