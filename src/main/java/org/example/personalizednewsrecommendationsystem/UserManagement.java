e95bef80fee73932f7ea539ece38c748b662084b
Your NameHirudika <you@example.comhirudikaanupama4@gmail.com>
2024-11-05 23:12:43 +0530

    Create logging page

--- src/main/java/org/example/personalizednewsrecommendationsystem/UserManagement.java
+++ src/main/java/org/example/personalizednewsrecommendationsystem/UserManagement.java
@@ -1,14 +1,19 @@
 package org.example.personalizednewsrecommendationsystem;
 
-
-
 public class UserManagement {
+    private Login_Scene_Controller controller;
 
+    // Constructor to set the controller
+    public UserManagement(Login_Scene_Controller controller) {
+        this.controller = controller;
+    }
 
+    public void check() {
+        String name = controller.getLoggingUserName().getText();
+        String password = controller.getLoggingUserPassword().getText();
 
-
-
-
-
-
+        if (name.isEmpty() || password.isEmpty()) {
+            controller.getLoggingMessage().setText("Empty");
+        }
+    }
 }
