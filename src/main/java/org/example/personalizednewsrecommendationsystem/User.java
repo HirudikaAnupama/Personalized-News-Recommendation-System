75e852571bb307980eeeb6adde6a62b9d9d67eb2
HirudikaAnupama <hirudikaanupama4@gmail.com>
2024-11-05 20:23:23 +0530

    Update DataBaseManagement class

--- src/main/java/org/example/personalizednewsrecommendationsystem/User.java
+++
@@ -1,4 +0,0 @@
-package org.example.personalizednewsrecommendationsystem;
-
-public class User extends UserManagement{
-}
--- src/main/java/org/example/personalizednewsrecommendationsystem/UserPreferences.java
+++
@@ -1,4 +0,0 @@
-package org.example.personalizednewsrecommendationsystem;
-
-public class UserPreferences extends UserManagement {
-}
